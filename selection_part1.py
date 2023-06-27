9671fb8a5f8910dac8cfbebbe6547da2f109f2c3
davidkapcsandi <david_kapcsandi@hotmail.com>
2023-06-27 16:19:52 +0100

    added another labs

--- selection_part1.py
+++
@@ -1,8 +0,0 @@
-age = int(input("How old are you?: "))
-
-if age >= 18:
-    print("You are in catefory A")
-elif age >= 16:
-    print( "You are in category B")
-else:
-    print("You are in category C")
